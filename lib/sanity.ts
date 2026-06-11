ee285bc105528ff502b75619c6bf5775588f23c4
Emmanuel Oduor <wyllademanu003@gmail.com>
2026-06-11 11:19:58 +0300

    Add Sanity client configuration

---
+++ lib/sanity.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  apiVersion: '2024-01-01',
+  useCdn: true,
+  perspective: 'published',
+})
+
+const builder = imageUrlBuilder(client)
+
+export function urlFor(source: unknown) {
+  return builder.image(source)
+}
